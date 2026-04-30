2bc89a6e5f7eaec511fa7c7d56cba240a9cf0e69
youngho00 <younghoo0619@gmail.com>
2026-04-30 11:54:35 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCD70RWw93q81d-JsLYLlaElIaJa4dAotI",
+  authDomain: "my-portfolio-15d82.firebaseapp.com",
+  projectId: "my-portfolio-15d82",
+  storageBucket: "my-portfolio-15d82.firebasestorage.app",
+  messagingSenderId: "227479400800",
+  appId: "1:227479400800:web:2eea5ef9255022973a2e05",
+  measurementId: "G-F0SFEP3LLY"
+};
+
+// Global export for board.html
+window.firebaseConfig = firebaseConfig;
+console.log("Firebase Config Updated to: my-portfolio-15d82");
